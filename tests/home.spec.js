7c1b5fbdb7779f325f6bdc63c47d4a5840a88dd6
David McGill <dmcgill50@gmail.com>
2025-06-02 13:11:24 -0700

    Add Playwright tests (#7)

---
+++ tests/home.spec.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@playwright/test');
+const path = require('path');
+
+const filePath = 'file://' + path.join(__dirname, '..', '_site', 'index.html');
+
+test('home page loads', async ({ page }) => {
+  await page.goto(filePath);
+  await expect(page).toHaveTitle(/David McGill Technologies/);
+  await expect(page.locator('h1')).toContainText('Welcome to David McGill Technologies');
+});
